0c0c3bc1be4c9a803a5a033299eeeec7a45206dc
GrantMan024 <46170244+GrantMan024@users.noreply.github.com>
2020-03-31 10:33:44 -0400

    Updated Code

--- AnalogLED.ino
+++ AnalogLED.ino
@@ -1,5 +1,5 @@
 //-----INFO-----
-  //Paul McWhorter tutorials
+  //Paul McWhorter Tutorials (https://www.patreon.com/PaulMcWhorter)
    //New Arduino Tutorial 7
    
   //Date : March 31 2020
@@ -23,7 +23,7 @@ int bright7    = 200  ;
 int bright8    = 225  ;
 int brightFull = 255  ;
 
-  //Delay Time
+  //Delay Times
 int dy1        = 0500 ;
 int dy2        = 2000 ;
 int dy3        = 3000 ;
@@ -33,12 +33,15 @@ int dy5        = 5000 ;
 //-----Setup-----
 
 void setup() {
-  pinMode(LED1, OUTPUT); //Swtting the first LED pin as Output
+    //Set LEDs as Outputs
+  pinMode(LED1, OUTPUT); //Setting the first LED pin as Output
 }
 
+//-----Loop-----
 void loop() {
-  analogWrite(LED1 , bright1) ;
-   delay(dy5)                 ;
+    //Analog write to the LEDs
+  analogWrite(LED1 , bright1) ; //Set LED Brightness
+   delay(dy5)                 ; //Delay 5 Seconds
    
   analogWrite(LED1 , bright5) ;
    delay(dy5)                 ;
